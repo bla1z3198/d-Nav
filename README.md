106868854c8c23c7072c4224c2f9b2bd3fbf310d
bla1z3198 <aguglov820@gmail.com>
2026-03-30 19:51:09 +0300

    v2

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 Program for creating and calculating trajectories of aircraft. 
 which contains curves and lines.
 
-# v1
+# v2
 
-Last version (v1) have commands.
-![dnav commands](/home/rzr/Pictures/dnav.png)
+Last version (v2) have commands and fixed geometry & algebra.
+* init [filename] - init json with filename
+* nav - calculate trajectory from current json
+* exit - exit from dnav
+
+* Fixed calculating of first point, and angles of circle sectors.
--- core/core.go
+++ core/core.go
@@ -20,6 +20,10 @@ func Nav(points []Point) { // Func for calculate length of way
 	var prev_cos float64
 	// Length of way
 	var result float64
+	// Length of first tangent
+	var tangent float64
+	// Distance between tangent points
+	var tangent_line float64
 	// Cycle for Points
 	for i := range points {
 		// Calculate length for pairs of points (not single)
@@ -33,25 +37,48 @@ func Nav(points []Point) { // Func for calculate length of way
 		By := points[i+1].Y
 		Radius := points[i].R
 		// Calculate length between A and B
-		line_len := math.Sqrt(math.Pow(Bx-Ax, 2) +
+		distanceAB := math.Sqrt(math.Pow(Bx-Ax, 2) +
 			math.Pow(By-Ay, 2))
 
-		sin := (By - Ay) / line_len
+		sin := (By - Ay) / distanceAB
 		cos := math.Sqrt(1 - sin*sin)
 		// Calculate length between tangent points
-		prepare := math.Pow((Ax-sin*Radius)-(Ax-prev_sin*Radius), 2) +
-			math.Pow((Ay+cos*Radius)-(Ay+prev_cos*Radius), 2)
+		if i == 0 {
+			// Distance between start and first point
+			help_distance := (math.Pow(Ax, 2) + math.Pow(Ay, 2))
+			// First tangent
+			tangent = math.Sqrt(help_distance -
+				(Radius * Radius))
+			// Angle between x axis and tangent
+			angle := math.Asin(Radius/math.Sqrt(help_distance)) +
+				math.Asin(Ay/math.Sqrt(help_distance))
+			// Coordinates of first point
+			x := math.Cos(angle) * tangent
+			y := math.Sin(angle) * tangent
+
+			tangent_line = math.Pow((Ax-sin*Radius)-(x), 2) +
+				math.Pow((Ay+cos*Radius)-(y), 2)
+		} else {
+			tangent_line = math.Pow((Ax-sin*Radius)-(Ax-prev_sin*Radius), 2) +
+				math.Pow((Ay+cos*Radius)-(Ay+prev_cos*Radius), 2)
+		}
 		// Argument for arccos of gamma angle
-		arg := ((prepare) / (2 * points[i].R * points[i].R)) - 1
+		arg := (1 - (tangent_line / (2 * Radius * Radius)))
 		gamma := math.Acos(arg)
-		gamma = ((gamma / math.Pi) * 180) - 180
+		if i > 0 {
+			if (points[i].Y < points[i-1].Y) &&
+				(points[i].Y < points[i+1].Y) {
+				gamma = 6.28319 - gamma
+			}
+		}
 		// Calculate length off circle sector
-		sector_len := (math.Pi * points[i].R * gamma) / 180
+		sector := Radius * gamma
 		// Result length of way
-		result += line_len + sector_len
+		result += distanceAB + sector
 		// Previous sin and cos
 		prev_sin = sin
 		prev_cos = cos
+		fmt.Println("Angle =", (gamma*180)/3.1415)
 	}
-	fmt.Println("Result length:", result)
+	fmt.Println("Result length:", result+tangent)
 }
